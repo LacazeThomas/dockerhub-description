49d2d912b05fb96975a5aa466639f0ad92cc8985
Thomas LACAZE <contact@thomaslacaze.fr>
2021-01-30 23:15:35 +0100

    fix drone compatibility

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,11 +3,4 @@ set -eo pipefail
 IFS=$'\n\t'
 
 # Execute the action code and output to file
-node index.js > action.log 2>&1
-
-# Remove lines containing sensitive information from the log
-sed -i '/::debug::/d' ./action.log
-sed -i '/::add-mask::/d' ./action.log
-
-# Output the log
-cat action.log
+node index.js
